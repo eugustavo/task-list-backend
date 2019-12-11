3de4dbfec382dd253617f2ca224b1bd2b25b7621
Gustavo Pereira de Souza <gustavo.souza@crp.com>
2019-12-11 13:15:51 -0300

    UserController alter functions

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     async update(req, res){
-        const { id } = req.headers;
+        const { user_id } = req.headers;
         const { email, password } = req.body;
 
         if( await User.findOne({ email: email })){
@@ -29,13 +29,19 @@ module.exports = {
         }
 
         if(!password){
-            await User.findByIdAndUpdate({ _id:id }, {
+            await User.findByIdAndUpdate({ _id:user_id }, {
                 email,
             });
             return res.status(200).json({ message: 'E-mail alterado com sucesso!'});
         }
+        if(!email){
+            await User.findByIdAndUpdate({ _id:user_id }, {
+                password,
+            });
+            return res.status(200).json({ message: 'E-mail alterado com sucesso!'});
+        }
 
-        await User.findByIdAndUpdate({ _id:id }, {
+        await User.findByIdAndUpdate({ _id:user_id }, {
             email,
             password
         });
@@ -44,9 +50,9 @@ module.exports = {
     },
 
     async destroy(req, res){
-        const { id } = req.headers;
+        const { user_id } = req.headers;
 
-        await User.findByIdAndDelete({ _id:id });
+        await User.findByIdAndDelete({ _id:user_id });
         return res.status(200).json({ message: 'Usuário deletado!' });
     }
 }
