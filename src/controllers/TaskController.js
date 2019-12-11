eecf34b21c74e1ab82437f022a2cea415575abe4
Gustavo Pereira de Souza <gustavo.souza@crp.com>
2019-12-11 09:12:20 -0300

    TaskController bug fixes

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -2,12 +2,12 @@ const Task = require('../models/Task');
 const User = require('../models/User');
 
 module.exports = {
-    async store(req, res){
+    async store(req, res) {
         const { user_id } = req.headers;
         const { name, description } = req.body;
 
-        if(!user_id){
-            return res.status(404).json({ message: 'Usuário não logado ou não existe!'});
+        if (!user_id) {
+            return res.status(404).json({ message: 'Usuário não logado ou não existe!' });
         }
         const response = await Task.create({
             name,
@@ -18,55 +18,57 @@ module.exports = {
         return res.status(200).json(response);
     },
 
-    async index(req, res){
+    async index(req, res) {
         const { user_id } = req.headers;
-        if(!user_id){
-            return res.status(404).json({ message: 'Usuário não logado ou não existe!'});
+        if (!user_id) {
+            return res.status(404).json({ message: 'Usuário não logado ou não existe!' });
         };
-        const response = await Task.find({ user:user_id }).populate('user');
+        const response = await Task.find({ user: user_id }).populate('user');
 
         return res.status(200).json(response);
     },
 
-    async update(req, res){
+    async update(req, res) {
         const { task_id, user_id } = req.headers;
         const { name, description } = req.body;
-        const aux = await Task.findOne({ user:user_id });
 
-        if(!task_id || !user_id){
-            return res.status(404).json({ message: 'Usuário não está logado ou Tarefa não existe!'});
+        if (!task_id || !user_id) {
+            return res.status(404).json({ message: 'Usuário não está logado ou Tarefa não existe!' });
         }
-        if(aux.user != user_id){
-            return res.status(400).json({ message: 'Usuário não é proprietario desta tarefa!'})
+
+        const aux = await Task.findById({ _id:task_id });
+       
+        if (!aux || (aux.user.toString() !== user_id.toString()) ) {
+            return res.status(400).json({ message: 'Usuário não é proprietario desta tarefa!' })
         }
         if(!name){
-            const response = await Task.findByIdAndUpdate({ _id:task_id }, { description });
-            return res.status(200).json(response);
+            await Task.findByIdAndUpdate({ _id:task_id }, { description });
+            return res.status(200).json({ message: 'Descrição alterada com sucesso!' });
         }
         if(!description){
-            const response = await Task.findByIdAndUpdate({ _id:task_id }, { name });
-            return res.status(200).json(response);
+            await Task.findByIdAndUpdate({ _id:task_id }, { name });
+            return res.status(200).json({ message: 'Nome da tarefa alterado com sucesso!' });
         }
 
-        const response = await Task.findByIdAndUpdate({ _id:task_id }, {
+        await Task.findByIdAndUpdate({ _id: task_id }, {
             name,
             description
         });
-        return res.status(200).json(response);
+        return res.status(200).json({ message: 'Tarefa alterada com sucesso!' });
     },
 
-    async destroy(req, res){
+    async destroy(req, res) {
         const { task_id, user_id } = req.headers;
-        const aux = await Task.findOne({ user:user_id });
+        const aux = await Task.findById({ _id:task_id });
 
-        if(!task_id || !user_id){
-            return res.status(404).json({ message: 'Usuário ou Tarefa não existem!'});
+        if (!task_id || !user_id) {
+            return res.status(404).json({ message: 'Usuário não está logado ou Tarefa não existe!' });
         }
-        if(aux.user != user_id){
-            return res.status(400).json({ message: 'Usuário não é proprietario desta tarefa!'});
+        if (aux.user.toString() !== user_id.toString()) {
+            return res.status(400).json({ message: 'Usuário não é proprietario desta tarefa!' });
         }
-        await Task.findByIdAndDelete({ _id:task_id });
-        return res.status(200).json({ message: 'Tarefa exluída com sucesso!'});
+        await Task.findByIdAndDelete({ _id: task_id });
+        return res.status(200).json({ message: 'Tarefa exluída com sucesso!' });
 
     }
 }
